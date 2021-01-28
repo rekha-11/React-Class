05442ec474789db9482730060155ca9b6e8322a4
Rekha Bhandari <rekhabhandaree77@gmail.com>
2021-01-28 12:15:40 +0545

    From video 1

--- src/App.js
+++ src/App.js
@@ -1,32 +1,61 @@
-import React, { Component } from 'react'
-import './App.css'
-import Navbar from './Navbar'
-import Contact from './Component2/Contact';
-import About from './Component2/About';
-import Shop from './Component2/Shop';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import React from 'react'
+import './App.css';
+import Tweet from './Tweet'
 
-export default class App extends Component {
-  render() {
-    return (
-      <Router>
-      <div className='App'>
-        <Navbar/>
-        <Switch>
-          <Route path='/' exact component={Home}/>
-          <Route path='/contact' component={Contact}/>
-          <Route path='/about' component={About}/>
-          <Route path='/shop' component={Shop}/>
-        </Switch>
-      </div>
-      </Router>
-    )
-  }
+export default function App() {
+return (
+  <div className='App'>
+    <Tweet/>
+    <Tweet/>
+    <Tweet/>
+  </div>
+);
 }
 
-const Home = () => (
-  <div>
-    <h1>Home</h1>
-  </div>
-)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import './App.css'
+// import Navbar from './Navbar'
+// import Contact from './Component2/Contact';
+// import About from './Component2/About';
+// import Shop from './Component2/Shop';
+// import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <Router>
+//       <div className='App'>
+//         <Navbar/>
+//         <Switch>
+//           <Route path='/' exact component={Home}/>
+//           <Route path='/contact' component={Contact}/>
+//           <Route path='/about' component={About}/>
+//           <Route path='/shop' component={Shop}/>
+//         </Switch>
+//       </div>
+//       </Router>
+//     )
+//   }
+// }
+
+// const Home = () => (
+//   <div>
+//     <h1>Home</h1>
+//   </div>
+// )
 
---
+++ src/Tweet.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+export default function Tweet() {
+    return (
+        <div className='tweet'>
+           <h3>Name:</h3>
+            <p>This is a random Tweet.</p>
+            <h3>Number of likes</h3>
+        </div>
+    );
+}
